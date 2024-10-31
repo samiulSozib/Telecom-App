58a94a7bcca9771ad73210b97fad68c665ffb185
Md Samiul <samiuljust2018@gmail.com>
2024-11-01 00:19:48 +0600

    update

--- src/app/utils/constant.js
+++ src/app/utils/constant.js
@@ -3,6 +3,6 @@ export const sideNavWidth = 260;
 export const navbarHeight = 60;
 export const sidenavCompactWidth = 80;
 export const containedLayoutWidth = 1200;
-//export const base_url='https://app-api-an-v1-24.afghannet.net/api/reseller'
+export const base_url='https://app-api-an-v1-24.afghannet.net/api/reseller'
 
-export const base_url='https://app-api-wt-v1-24.watantelecom.com/api/reseller'
+//export const base_url=''
